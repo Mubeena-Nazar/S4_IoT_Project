60a3a18713082c90398588aba1acfc5e852fd026
Mubeena Nazar <74751744+Mubeena-Nazar@users.noreply.github.com>
2022-06-12 14:09:13 +0530

    Create MainActivity.java

---
+++ App/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.smart_stick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton main_b1,main_b2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        main_b1=findViewById(R.id.main_b1);
+        main_b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, " NEXT ", Toast.LENGTH_SHORT).show();
+                Intent i=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(i);
+            }
+        });
+
+        main_b2=findViewById(R.id.main_b2);
+        main_b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, " find now ", Toast.LENGTH_SHORT).show();
+                Intent i=new Intent(MainActivity.this,MainActivity5.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    public void radioHandle(View view) {
+    }
+}
