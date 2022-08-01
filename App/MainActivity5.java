b6fcf9dba06fda78fee25895f31b8792416f1b92
Mubeena Nazar <74751744+Mubeena-Nazar@users.noreply.github.com>
2022-08-01 11:54:30 +0530

    Update MainActivity5.java

--- App/MainActivity5.java
+++ App/MainActivity5.java
@@ -1,33 +1,102 @@
-package com.example.smart_stick;
+package com.example.smart_blind_stick;
+
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
+import java.io.IOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity5 extends AppCompatActivity {
 
-    Button find_b1,find_b2;
+    private TextView mTextViewResult;
+    Button b1,b2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
 
-        find_b1=findViewById(R.id.find_b1);
-        find_b1.setOnClickListener(new View.OnClickListener() {
+
+        b1=findViewById(R.id.find_b1);
+        b2=findViewById(R.id.find_b2);
+        OkHttpClient client = new OkHttpClient();
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity5.this, " ON ", Toast.LENGTH_SHORT).show();
+
+                String url = "http://192.168.1.2/on_buzzer\n";                                      // --------- IP address --------
+
+
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        if (response.isSuccessful()) {
+                            final String myResponse = response.body().string();
+
+                            MainActivity5.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    //mTextViewResult.setText(myResponse);
+                                    Toast.makeText(MainActivity5.this, "Done", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }
+                });
             }
         });
 
-        find_b2=findViewById(R.id.find_b2);
-        find_b2.setOnClickListener(new View.OnClickListener() {
+        b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity5.this, " OFF ", Toast.LENGTH_SHORT).show();
+
+                String url = "http://192.168.1.2/off_buzzer\n";                                     // --------- IP address --------
+
+
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        if (response.isSuccessful()) {
+                            final String myResponse = response.body().string();
+
+                            MainActivity5.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mTextViewResult.setText(myResponse);
+                                    Toast.makeText(MainActivity5.this, "Done", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }
+                });
             }
         });
     }
